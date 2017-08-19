a8d760f374c7fd3fea54dd85380a474ef42ec71c
Nish <nish@Sowdris-MBP.lan>
2017-08-19 16:07:25 +0930

    Updating package.json

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log("Let's sail!");
+export { default as AnimatedScreen } from './build/components/AnimatedScreen';
+export { default as AnimatedRoute } from './build/components/AnimatedRoute';
+export { default as AnimationContainer } from './build/components/AnimationContainer';
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AnimatedScreen } from './components/AnimatedScreen';
-export { default as AnimatedRoute } from './components/AnimatedRoute';
-export { default as AnimationContainer } from './components/AnimationContainer';
